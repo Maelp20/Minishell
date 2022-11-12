76ba7819b6a6e39c049ad6d6ae38a2b2279cd593
Yannis THOMAS <yanthoma@e2r9p1.clusters.42paris.fr>
2022-11-12 12:53:55 +0100

    adding quote

--- srcs/lexer_spe_char.c
+++ srcs/lexer_spe_char.c
@@ -6,7 +6,7 @@
 /*   By: yanthoma <yanthoma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 08:25:04 by yanthoma          #+#    #+#             */
-/*   Updated: 2022/10/30 19:34:37 by yanthoma         ###   ########.fr       */
+/*   Updated: 2022/11/12 12:42:17 by yanthoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,23 @@
 
 int	is_in_quote(char *arg)
 {
-	int i;
+	int	i;
+	int	dbq;
+	int	q;
 	
 	i = 0;
+	dbq = 0;
+	q = 0;
 	while (arg[i])
 	{
-		if (arg[i] == '\"')
-			printf("un \"");
-		i++;
+		if (arg[i] == '\"' && dbq == 0 && q < 2)
+			dbq++;
+		else if (arg[i] == '\'' && dbq == 0)
+			q++;
+		else if (arg[i] == '\"' && dbq == 1)
+			return (1);
+		else if (arg[i])
+		
 	}
 	return(0);
 }
