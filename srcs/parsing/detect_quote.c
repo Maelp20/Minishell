4a1b2183ba1a7de36e815aa1f7bf1cedb8628496
Yannis THOMAS <yanthoma@e3r4p23.clusters.42paris.fr>
2022-12-05 18:18:27 +0100

    moving floor

--- srcs/parsing/detect_quote.c
+++ srcs/parsing/detect_quote.c
@@ -6,7 +6,7 @@
 /*   By: yanthoma <yanthoma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 08:25:04 by yanthoma          #+#    #+#             */
-/*   Updated: 2022/12/05 01:00:03 by yanthoma         ###   ########.fr       */
+/*   Updated: 2022/12/05 18:16:21 by yanthoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,11 +45,38 @@ int	split_dbq(char *input, int i, t_tok **lst)
 	return (free(tmp), ++i);
 } 
 
+int	split_with_sq_dollar(char *input, int i, t_tok **lst)
+{
+	int j;
+	char *tmp;
+
+	j = i;
+	while (input[j] != '\'')
+	{
+		printf("split dollar %c\n", input[j]);
+		j++;
+	}
+	printf("split dollar %c\n", input[j]);
+	printf("j = %d\n", j - i + 1);
+	tmp = malloc(sizeof(char *) * (j - i + 1));
+	if (!tmp)
+		return(-2);
+	j = -1;
+	i--;
+	while (input[++i] && input[i] != '\'')
+		tmp[++j] = input[i];
+	printf("%s\n", tmp);
+	lstadd_back_token(lst, lstnew_token(tmp));
+	return (free(tmp), ++i);
+}
+
 int	split_sq(char *input, int i, t_tok **lst)
 {
 	int		j;
 	char	*tmp;
 	
+	if (input[i] == '$')
+		return (split_with_sq_dollar(input, i, lst));
 	j = i;
 	while (is_sep(input[j]) != 2 && input[j + 1])
 		j++;
