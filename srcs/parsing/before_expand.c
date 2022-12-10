ffce34eb41bdc10b2ff759912b380a277637f6b9
Yannis THOMAS <yanthoma@e1r4p14.clusters.42paris.fr>
2022-12-10 15:48:19 +0100

    parsing started with segfault

--- srcs/parsing/before_expand.c
+++ srcs/parsing/before_expand.c
@@ -6,7 +6,7 @@ int	is_separator(char *str)
 	int i;
 
 	i = 0;
-	if (str[i] == '|' || str[i] == '>' || str[i] == '<')
+	if (str[i] == '|')
 		return (1);
 	else 
 		return (0);
@@ -24,25 +24,26 @@ int count_non_separators(t_tok *lst)
 	return count;
 }
 
-char **extract_args(t_tok **lst, int pos)
+char **extract_args(t_tok *lst, int pos)
 {
 	int i; 
 	char **dest;
 
 	i = 0;
 	dest = malloc(sizeof(char *) * pos);
-	while ((*lst) && i < pos)
+	while (lst && i < pos)
 	{
-		dest[i] = ft_strdup((*lst)->token);
-		(*lst) = (*lst)->next;
+		printf("extract token %s\n", lst->token);
+		dest[i] = ft_strdup(lst->token);
+		lst = lst->next;
 		i++;
 	}
+	dest[pos]= '\0';
 	return (dest);
 }
 
 void	split_lst_operator(t_tok **lst, t_data **data)
 {
-	(void)data;
 	int	pos;
 	pos = 0;
 	// while((*lst))
@@ -50,6 +51,28 @@ void	split_lst_operator(t_tok **lst, t_data **data)
 	//  	printf("%s\n", (*lst)->token);
 	//  	(*lst) = (*lst)->next;
 	// }
-	pos = count_non_separators((*lst));
-
+	while((*lst))
+	{
+		pos = count_non_separators((*lst));
+		printf("%d\n", pos);
+	 	if (pos)
+		{
+			(*data)->args = extract_args((*lst), pos);
+			while (--pos)
+				(*lst) = (*lst)->next;
+		}
+		pos = 0;
+	 	(*lst) = (*lst)->next;
+	}
+	int i;
+	while((*data))
+	{
+	 	i = 0;
+		while ((*data)->args[i])
+		{
+			printf("%s\n", (*data)->args[i]);
+			i++;
+		}
+	 	(*data) = (*data)->next;
+	}
 }
