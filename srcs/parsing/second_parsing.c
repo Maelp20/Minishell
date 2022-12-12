7c58b270bcf792ce222970200056520a0ae7cc2b
yanthoma <yanthoma@student.42.fr>
2022-12-13 00:31:32 +0100

    try fixing the spaces before and after

--- srcs/parsing/second_parsing.c
+++ srcs/parsing/second_parsing.c
@@ -97,100 +97,193 @@ int	is_to_split(char c)
 // 	}
 // }
 
+// t_tok *split_string(const char *str)
+// {
+//     t_tok *head = NULL;
+//     t_tok *current = NULL;
+//     int i = 0;
+//     int token_start = 0;
+//     int token_length = 0;
+//     int prev_was_delimiter = 0;
+
+//     while (str[i])
+//     {
+//         // Check if the current character is one of the delimiters
+//         if (str[i] == '|' || str[i] == '>' || str[i] == '<')
+//         {
+//             // If the current token is not empty and the previous token was not a delimiter, add it to the list
+// 			printf("str[%d] = %c\n",i, str[i]);
+//             if (token_length > 0 && !prev_was_delimiter)
+//             {
+//                 if (head)
+//                 {
+//                     current->next = malloc(sizeof(t_tok));
+//                     current = current->next;
+//                 }
+//                 else
+//                 {
+//                     head = current = malloc(sizeof(t_tok));
+//                 }
+
+//                 // Copy the current token to the new t_tok structure
+//                 current->token = malloc(token_length + 1);
+//                 memcpy(current->token, str + token_start, token_length);
+//                 current->token[token_length] = '\0';
+//                 current->next = NULL;
+
+//                 // Reset the token start and length for the next token
+//                 token_start = i + 1;
+//                 token_length = 0;
+//             }
+//             // Create a new token for the delimiter
+//          else
+// 			{
+// 				// Check if the current token is not empty
+// 				if (token_length > 0)
+// 				{
+// 					if (head)
+// 					{
+// 						current->next = malloc(sizeof(t_tok));
+// 						current = current->next;
+// 					}
+// 					else
+// 					{
+// 						head = current = malloc(sizeof(t_tok));
+// 					}
+// 					current->token = malloc(2);
+// 					current->token[0] = str[i];
+// 					current->token[1] = '\0';
+// 					current->next = NULL;
+// 				}
+// 				prev_was_delimiter = 1;
+// 			}
+//         }
+//         else
+//         {
+//             // If the current character is not a delimiter, increment the length of the current token
+//             token_length++;
+//             prev_was_delimiter = 0;
+//         }
+//         i++;
+//     }
+//     // Add the final token to the list if it is not empty and the previous token was not a delimiter
+//     if (token_length > 0 && !prev_was_delimiter)
+//     {
+//         if (head)
+//         {
+//             current->next = malloc(sizeof(t_tok));
+//             current = current->next;
+//         }
+//         else
+//         {
+//             head = current = malloc(sizeof(t_tok));
+//         }
+//         current->token = malloc(token_length + 1);
+// 		memcpy(current->token, str + token_start, token_length);
+// 		current->token[token_length] = '\0';
+// 		current->next = NULL;
+// 	}
+// 	return head;
+// }
+
+
 t_tok *split_string(const char *str)
 {
-    t_tok *head = NULL;
-    t_tok *current = NULL;
-    int i = 0;
-    int token_start = 0;
-    int token_length = 0;
-    int prev_was_delimiter = 0;
+  // Create a linked list to store the tokens
+  t_tok *list = NULL;
+  t_tok *current = NULL;
 
-    while (str[i])
+  // Create a temporary buffer to hold the current token
+  char buf[strlen(str) + 1];
+  int buf_pos = 0;
+
+  // Loop through the input string
+  for (size_t i = 0; i < strlen(str); i++)
+  {
+    char c = str[i];
+
+    // Check if the current character is a separator
+    if (c == '>' || c == '<' || c == '|' || (i < strlen(str) - 1 && str[i] == str[i + 1]))
     {
-        // Check if the current character is one of the delimiters
-        if (str[i] == '|' || str[i] == '>' || str[i] == '<')
-        {
-            // If the current token is not empty and the previous token was not a delimiter, add it to the list
-			printf("str[%d] = %c\n",i, str[i]);
-            if (token_length > 0 && !prev_was_delimiter)
-            {
-                if (head)
-                {
-                    current->next = malloc(sizeof(t_tok));
-                    current = current->next;
-                }
-                else
-                {
-                    head = current = malloc(sizeof(t_tok));
-                }
+      // Add the separator as a token
 
-                // Copy the current token to the new t_tok structure
-                current->token = malloc(token_length + 1);
-                memcpy(current->token, str + token_start, token_length);
-                current->token[token_length] = '\0';
-                current->next = NULL;
+      // Add the current token to the list
+      buf[buf_pos] = '\0';
+      t_tok *token = malloc(sizeof(t_tok));
+      token->token = strdup(buf);
+      token->next = NULL;
+      if (current)
+      {
+        current->next = token;
+        current = current->next;
+      }
+      else
+      {
+        list = token;
+        current = token;
+      }
 
-                // Reset the token start and length for the next token
-                token_start = i + 1;
-                token_length = 0;
-            }
-            // Create a new token for the delimiter
-         else
-			{
-				// Check if the current token is not empty
-				if (token_length > 0)
-				{
-					if (head)
-					{
-						current->next = malloc(sizeof(t_tok));
-						current = current->next;
-					}
-					else
-					{
-						head = current = malloc(sizeof(t_tok));
-					}
-					current->token = malloc(2);
-					current->token[0] = str[i];
-					current->token[1] = '\0';
-					current->next = NULL;
-				}
-				prev_was_delimiter = 1;
-			}
-        }
-        else
-        {
-            // If the current character is not a delimiter, increment the length of the current token
-            token_length++;
-            prev_was_delimiter = 0;
-        }
-        i++;
-    }
-    // Add the final token to the list if it is not empty and the previous token was not a delimiter
-    if (token_length > 0 && !prev_was_delimiter)
-    {
-        if (head)
-        {
-            current->next = malloc(sizeof(t_tok));
-            current = current->next;
-        }
-        else
-        {
-            head = current = malloc(sizeof(t_tok));
-        }
-        current->token = malloc(token_length + 1);
-		memcpy(current->token, str + token_start, token_length);
-		current->token[token_length] = '\0';
-		current->next = NULL;
-	}
-	return head;
+      // Clear the buffer
+      buf_pos = 0;
+
+      // Create a new token for the separator
+      t_tok *separator = malloc(sizeof(t_tok));
+      separator->token = malloc(3);
+      if (i < strlen(str) - 1 && str[i] == str[i + 1])
+      {
+        // Handle double separators
+        separator->token[0] = c;
+        separator->token[1] =
+
+c;
+separator->token[2] = '\0';
+i++; // Skip the next character
+}
+else
+{
+// Handle single separators
+separator->token[0] = c;
+separator->token[1] = '\0';
+}
+separator->next = NULL;
+
+
+  // Add the separator to the list
+  if (current)
+  {
+    current->next = separator;
+    current = current->next;
+  }
+  else
+  {
+    list = separator;
+    current = separator;
+  }
+}
+else
+{
+  // Add the character to the current token buffer
+  buf[buf_pos] = c;
+  buf_pos++;
+}
 }
 
+// Add the last token to the list
+buf[buf_pos] = '\0';
+t_tok *token = malloc(sizeof(t_tok));
+token->token = strdup(buf);
+token->next = NULL;
+if (current)
+{
+current->next = token;
+}
+else
+{
+list = token;
+}
 
-
-
-
-
+return list;
+}
 
 
 void	clean_token_lst(t_tok **lst)
