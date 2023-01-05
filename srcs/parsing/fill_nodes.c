564a2e685601182a8554c75a3cced9a6ca31112c
Yannis THOMAS <yanthoma@e2r11p10.clusters.42paris.fr>
2023-01-05 21:45:36 +0100

    refacto lol

--- srcs/parsing/fill_nodes.c
+++ srcs/parsing/fill_nodes.c
@@ -1,38 +1,24 @@
 #include "exec.h"
 
 
-void	count_nodes(t_tok *lst, t_data **data)
+int	count_nodes(t_tok **lst)
 {
 	t_tok	*temp;
-	t_data	*head;
-	t_data	*node;
-	int		nb_nodes;	
+	int		nb_nodes;
 
-	nb_nodes =0;
-	temp = lst;
-	head = NULL;
-	*data = &head;
+	*lst = temp;
 	while (temp)
 	{
 		if(!ft_strcmp(temp->token, "|"))
 			nb_nodes++;
 		temp = temp->next;
-	}
-	if (nb_nodes > 1)
-		(*data)->outpipe = 1;
-	while (nb_nodes > 0)
-	{
-		node = ft_calloc(1, sizeof(t_data));
-		ft_bzero(node, sizeof(t_data));
-
-	}
-	
+	}	
 }
 
 void	fill_node_with_tok(t_tok **lst, t_data **data)
 {
 	int		nb_nodes;	
 
-	
+	nb_nodes= count_nodes(lst);
 	
 }
