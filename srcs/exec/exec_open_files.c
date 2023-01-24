41bd59ba085d1e390ea96d9ba522b7b3edd08a10
Mael PIGNET <you@example.com>
2023-01-24 19:42:05 +0100

    fixed conditional jump from stat()

--- srcs/exec/exec_open_files.c
+++ srcs/exec/exec_open_files.c
@@ -6,7 +6,7 @@
 /*   By: mpignet <mpignet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 16:08:37 by mpignet           #+#    #+#             */
-/*   Updated: 2023/01/24 18:14:45 by mpignet          ###   ########.fr       */
+/*   Updated: 2023/01/24 19:34:24 by mpignet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ int	ft_open_infile(t_data *data)
 	}
 	else
 	{
-		if (check_if_dir(data->infile))
+		if (check_if_dir(data->infile, data))
 			return (msg_is_directory(data->infile), 1);
 		data->in_fd = open(data->infile, O_RDONLY);
 		if (data->in_fd < 0)
@@ -69,7 +69,7 @@ int	ft_open_outfile(t_data *data)
 	}
 	else
 	{
-		if (check_if_dir(data->outfile))
+		if (check_if_dir(data->outfile, data))
 			return (msg_is_directory(data->outfile), 1);
 		data->out_fd = open(data->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		if (data->out_fd < 0)
